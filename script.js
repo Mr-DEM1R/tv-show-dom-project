4c850f38cb1d75013b26713b651da5dabf783548
Mr.Demir <mehmetomer806@gmail.com>
2023-06-06 09:48:03 +0100

    m

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //You can edit ALL of the code here
-function setup() {
+/* function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
@@ -7,6 +7,36 @@ function setup() {
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+} */
+
+//window.onload = setup;
+
+/* const promise1= new Promise((resolve, reject) => {
+  resolve('data')
+})
+console.log(promise1) */
+
+/// SECOND ONE
+
+const ROOT = document.getElementById("root");
+const SHOWS = getAllShows();
+
+function startPage() {
+  SHOWS.forEach((show) => {
+    const CARD = document.createElement("div");
+    CARD.className = 'show-card'
+    const showName = document.createElement('h1');
+    showName.innerText = show.name
+    const image = document.createElement('img');
+    image.src = show.image.medium
+    const summary = document.createElement('p');
+    summary.innerText = show.summary;
+    CARD.appendChild(showName);
+    CARD.appendChild(image)
+    CARD.appendChild(summary);
+    ROOT.appendChild(CARD)
+
+  });
 }
 
-window.onload = setup;
+window.onload = startPage();
